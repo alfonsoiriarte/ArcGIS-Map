b650bf4e847f87defbe224bd9bb41331afa482e4
Alfonso Iriarte <alfonsoiriarte02@gmail.com>
2022-04-26 17:02:30 -0300

    Add: Funcionalidad 3, extra credit 1 y 3

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let pointsArray = [];
+/* Inicializo array de prueba */
+let pointsArray = [
+    {
+    "pointId":0,
+    "nombre":"AEROTERRA S.A.",
+    "direccion":"Av. Eduardo Madero 1020",
+    "telefono":"54 9 11 5272 0900",
+    "category":"Comercial",
+    "latitud":"-34.595986",
+    "longitud":"-58.3724715"
+}
+];
 /* Configuro el mapa e importo dependencias */
 function handleMap () {
     require(["esri/config",
@@ -6,62 +17,190 @@ function handleMap () {
     "esri/views/MapView",
     "esri/Graphic",
     "esri/layers/GraphicsLayer",
-    ], function (esriConfig, Map, MapView, Graphic, GraphicsLayer) {
+    "esri/layers/FeatureLayer",
+    "esri/widgets/Search"
+    ], function (esriConfig, Map, MapView, Graphic, GraphicsLayer, FeatureLayer, Search) {
     /* Seteo Api Key */
     esriConfig.apiKey = "AAPK6c3e09a7857b4a01804a74239ff414aeIjVfG8LY5TvnzHY4FYcp-lIOSuLeWuT5SgtemI1fnAVqSnCPajQifw8WmwtgeQNp";
-    /* Creo un mapa con "modo" de mapa" arcgis-topographic */
+    /* Creo un mapa arcgis-navigation */
     const map = new Map({
-    basemap: "arcgis-topographic"
+        basemap: "arcgis-navigation"
     });
-
+    /* Seteo los parametros iniciales del mapa */
     const view = new MapView({
-    map: map,
-    center: [-58.368158, -34.627152],   /* Parametros:  Longitud, Latitud */
-    zoom: 1,                            /* Nivel de zoom */
-    container: "viewDiv"                /* Div contenedor de mapa */
+        map: map,
+        center: [-58.368158, -34.627152],       /* Parametros:  Longitud, Latitud */
+        zoom: 10,                               /* Nivel de zoom */
+        container: "viewDiv"                    /* Div contenedor de mapa */
     });
-
+    /* Añado busqueda inteligente */
+    const search = new Search({
+        view: view
+    });
+    view.ui.add(search, "top-right");
     const graphicsLayer = new GraphicsLayer();
     map.add(graphicsLayer);
-
-    pointsArray.map(data => {
-        const point =  { 
+    /* Mapeo array de puntos */
+    var graphics = pointsArray.map(function (place) {
+        return new Graphic({
+          attributes: {
+            name: place.nombre,
+            markerId: place.id,
+            address: place.direccion,
+            phone: place.telefono,
+            category: place.category,
+            coordenates: place.longitud + ", " + place.latitud
+          },
+          geometry: {
             type: "point",
-            longitude: data.longitud,
-            latitude: data.latitud
-        };
-        const simpleMarkerSymbol = {
+            longitude: place.longitud,
+            latitude: place.latitud
+          },
+          symbol: {
             type: "simple-marker",
             color: [226, 119, 40],
             outline: {
-                color: [255, 255, 255],
-                width: 1
+              color: [255, 255, 255],
+              width: 2
             }
-        };
-        /* Creo un grafico seteando las propiedades */
-        const pointGraphic = new Graphic({
-            geometry: point,
-            symbol: simpleMarkerSymbol
+          }
         });
-        graphicsLayer.add(pointGraphic);
-    })
-
+    });
+    /* Creo template de popup y añado el layer al grafico */
+    const featureLayer = new FeatureLayer({
+        source: graphics,
+        fields: [
+            {
+                name: "name",
+                alias: "name",
+                type: "string"
+            },
+            {
+                name: "markerId",
+                alias: "markerId",
+                type: "oid"
+            },
+            {
+                name: "address",
+                alias: "address",
+                type: "string"
+            },
+            {
+                name: "phone",
+                alias: "phone",
+                type: "string"
+            },
+            {
+                name: "category",
+                alias: "category",
+                type: "string"
+            },
+            {
+                name: "coordenates",
+                alias: "coordenates",
+                type: "string"
+            },
+        ],
+        objectIdField: "markerId",  
+        geometryType: "point",
+        renderer: {
+            type: "simple", 
+            symbol: {
+                type: "simple-marker",
+                size: 6,
+                color: "black",
+                outline: {
+                    width: 0.5,
+                    color: "white"
+                }
+            }
+        },
+        popupTemplate: {
+            title: "{name}",
+            content: [{
+                type: "fields",
+                fieldInfos: [
+                  {
+                    fieldName: "address",
+                    label: "Direccion",
+                    visible: true
+                  },
+                  {
+                    fieldName: "phone",
+                    label: "Telefono",
+                    visible: true
+                  },
+                  {
+                    fieldName: "category",
+                    label: "Categoria",
+                    visible: true
+                  },
+                  {
+                    fieldName: "coordenates",
+                    label: "(X,Y)",
+                    visible: true
+                  }
+                ]
+              }],
+            actions: [{
+                title: "Eliminar marcador",
+                id: "delete-marker",
+                image: "remove.png"
+            }]
+        }
+    });
+    /* Capturo accionamiento de boton eliminador de marcadores
+        y elimino el marcador actual */
+    view.popup.on("trigger-action", (event) => {
+        if (event.action.id === "delete-marker") {
+            let idActual = view.popup.selectedFeature.attributes.markerId - 1;
+            pointsArray.splice(idActual, 1);
+            handleMap();
+        }
+    });
+    map.layers.add(featureLayer);
 });
 }
 /* Inicializo el mapa */
 handleMap();
-
-/* Import dependecia para manejar archivo json (File System) */
-/* var fs = require('./fs');
-var http = require('http'); */
 
 /* Evito el refresco de paginal cuando se envíe el form */
 var form = document.getElementById("formMapId");
 function handleForm(event) { event.preventDefault(); } 
 form.addEventListener('submit', handleForm);
 
-/* Contador de marcadores */
+/* Inicializo contador de marcadores en 1 porque ya hay un ejemplo default
+ y seteo el select para eliminar marcadores */
 let counter = 0;
+/* Creo funcion para actualizar select eliminador de marcadores */
+function fillSelector(pointsArray) {
+    document.getElementById("marcadoresId").innerHTML = "<option>Seleccionar</option>";	
+    var modelList = document.getElementById("marcadoresId"); 
+    for (var i in pointsArray) { 
+        // creamos un elemento de tipo option
+        var opt = document.createElement("option");
+        // le damos un valor
+        opt.value = pointsArray[i].nombre;
+        // le ponemos un texto
+        opt.textContent = pointsArray[i].nombre;
+        // lo agregamos al select
+        modelList.options.add(opt);
+    }
+} 
+/* Inicializo el select con el ejemplo */
+fillSelector(pointsArray); 
+function deleteMarkerFromButton(){
+    let nameToDelete = document.getElementById("marcadoresId").value;
+    let indexArray = pointsArray.findIndex(objeto => {
+        return objeto.nombre === nameToDelete
+    })
+    pointsArray.splice(indexArray, 1);
+    console.log(pointsArray);
+    /* Recargo mapa y select */
+    handleMap();
+    fillSelector(pointsArray);
+}
+
 
 /* Valido los input del form */
 function validate() {
@@ -70,7 +209,6 @@ function validate() {
     let direccionInput = document.formMap.direccionInput;
     let telefonoInput = document.formMap.telefonoInput;
     let categoryInput = document.formMap.categoryInput;
-    let coordenadasInputInt = parseInt(document.formMap.coordenadasInput.value.split(',')[0])
     let coordenadasInput = document.formMap.coordenadasInput;
     let coordenadasArray = coordenadasInput.value.split(",",2)
     /* Valido inputs de form */
@@ -99,6 +237,7 @@ function validate() {
         document.getElementsByName('coordenadasInput')[0].placeholder='Campo obligatorio';
         return false;
     }
+    counter++;
 
     console.log(coordenadasArray);
     /* Guardo data en array pointsArray */
@@ -111,8 +250,17 @@ function validate() {
         "latitud": coordenadasArray[0],
         "longitud": coordenadasArray[1]
     });
+    pointsArray.map(marcador => {
+        document.getElementById("marcadoresId").innerHTML = "<option value="+ marcador.nombre +">" + marcador.nombre + "</option>"
+    })
+    /* Actualizo select para eliminar marcadores */
+    fillSelector(pointsArray); 
+    /* Creo el mapa con nuevo marcador */
     handleMap();
-    console.log(pointsArray);
-    counter++;
     return true;
+}
+
+
+function eliminarMarcador () {
+    console.log(document.getElementById("marcadoresId").value)
 }
